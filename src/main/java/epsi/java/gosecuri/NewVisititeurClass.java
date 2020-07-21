52c9e2b98f63bb99e7b8c7441b4ef51ba8441292
h-benyoussef <67738039+h-benyoussef@users.noreply.github.com>
2020-07-21 13:33:14 +0200

    Add files via upload

---
+++ src/main/java/epsi/java/gosecuri/NewVisititeurClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epsi.java.gosecuri;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Fayçal Ziba
+ */
+public class NewVisititeurClass {
+    
+    private String _nom;
+   private String _prenom;
+   private Date _dateNaissance;
+   private String _photoVisiteur;
+   private int _numeroCarteIdentite;
+   
+   public NewVisititeurClass (String nom, String prenom, Date dateNaissance, String photoVisiteur, int numeroCarteIdentite){
+       
+   _nom= nom;
+    _prenom= prenom;
+    _dateNaissance=dateNaissance;
+    _numeroCarteIdentite= numeroCarteIdentite;
+   
+   }
+   }
+    
+
